fabea37e915b4c3d5f6af0bac71ee515a9d210c3
icy <kinglioniod@gmail.com>
2025-11-19 00:44:08 -0400

    Initial NixOS + Home Manager flake config

--- home/icy/dev.nix
+++ home/icy/dev.nix
@@ -16,12 +16,36 @@
     terraform
   ];
 
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "icy";
+    userEmail = "kinglioniod@gmail.com";
+
+    extraConfig = {
+      init.defaultBranch = "main";
+      pull.rebase        = "false";
+      push.autoSetupRemote = "true";
+
+      core.editor = "nvim";
+
+      diff.colorMoved = "default";
+
+      core.autocrlf = "input";
+
+      status.submoduleSummary = "true";
+
+      credential.helper = "cache --timeout=7200";
+    };
+  };
 
   programs.neovim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
+
+    extraConfig = ''
+	inoremap jj <Esc>
+    '';
   };
 }
 
