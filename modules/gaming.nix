8d083d47122abc9fec11a41018c06e8b8dbba667
icy <kinglioniod@gmail.com>
2025-12-22 21:44:11 -0400

    rest of the gaming shit

---
+++ modules/gaming.nix
@@ -0,0 +1,25 @@
+{pkgs, ...}: {
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+  };
+
+  programs.gamemode.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+    protonup-ng
+    lutris
+    bottles
+    heroic
+    wineWowPackages.stable
+    wine
+    (wine.override {wineBuild = "wine64";})
+    wine64
+    wineWowPackages.staging
+    winetricks
+    wineWowPackages.waylandFull
+  ];
+
+  environment.sessionVariables.STEAM_EXTRA_COMPAT_TOOLS_PATHS = "$HOME/.steam/root/compatibilitytools.d";
+}
