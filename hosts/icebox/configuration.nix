634347e07b7cf1327b6128e01e9b7dbd53b1775f
icy <kinglioniod@gmail.com>
2026-04-19 08:43:57 -0400

    set monitor settings

--- hosts/icebox/configuration.nix
+++ hosts/icebox/configuration.nix
@@ -10,7 +10,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_6_18;
 
   # AMD GPU stability fixes
   boot.kernelParams = [
